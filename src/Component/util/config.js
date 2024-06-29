b0489cc1c2d4d34849524c07c5158cb954b9f19d
MAmmarRaza <20ntucs1120@student.ntu.edu.pk>
2024-06-29 06:10:47 +0000

    url updated

--- src/Component/util/config.js
+++ src/Component/util/config.js
@@ -1,5 +1,5 @@
 // export const baseURL = "https://the-app-backend-c2780c2bf830.herokuapp.com/";
-export const baseURL = "http://localhost:5000/";
+export const baseURL = "https://backend.bistail.com/";
 export const key = "";
 export const ProjectName = " b i s t a i l";
 // new
